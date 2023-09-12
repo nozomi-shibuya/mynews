fdd04c113e77b1219c79b8d6291fb5f337c685ee
Nozomi <4kinoko.non@gmail.com>
2023-09-12 14:27:24 +0000

    resourcesをコミット

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -11,3 +11,14 @@
         <h1>自己紹介作成画面</h1>
     </body>
 </html>
+@extends('layouts.admin')
+@section('title', '自己紹介の新規作成')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>自己紹介の新規作成</h2>
+            </div>
+        </div>
+    </div>
+@endsection
