bf8e9166c8c486581a5446ce90c2cafcb6c19aa6
Nozomi <4kinoko.non@gmail.com>
2023-09-14 07:27:08 +0000

    Laravel08の課題提出

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,16 @@ Route::get('/', function () {
 });
 
 use App\Http\Controllers\Admin\NewsController;
-Route::controller(NewsController::class)->prefix('admin')->group(function() {
-    Route::get('news/create', 'add')->middleware('auth');
+Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function () {
+    Route::get('news/create', 'add')->name('news.add');
+    Route::post('news/create', 'create')->name('news.create');
 });
 
 use App\Http\Controllers\Admin\ProfileController;
-Route::controller(ProfileController::class)->prefix('admin')->group(function() {
-    Route::get('profile/create', 'add')->middleware('auth');
-    Route::get('profile/edit', 'edit')->middleware('auth');
+Route::controller(ProfileController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function() {
+    Route::get('profile/create', 'add')->name('profile.add');
+    Route::post('profile/edit', 'edit')->name('profile.edit');
+    Route::put('profile/edit', 'update')->name('profile.update');
 });
 
 Auth::routes();
