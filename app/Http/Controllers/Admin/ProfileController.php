e8b58f64479930620437cdffc9354d942184976d
Nozomi <4kinoko.non@gmail.com>
2023-09-24 08:42:26 +0000

    Laravel12の課題

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -6,14 +6,17 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use App\Models\Profile;
+use App\Models\ProfileHistory;
+use Carbon\Carbon;
 
 class ProfileController extends Controller
 {
-    //
+
     public function add()
     {
         return view('admin.profile.create');
     }
+    
     public function create(Request $request)
     {
         $this->validate($request, Profile::$rules);
@@ -35,8 +38,25 @@ class ProfileController extends Controller
         return view('admin.profile.edit', ['profile_form' => $profile]);
     }
     
-    public function update()
+    public function update(Request $request)
     {
-        return redirect('admin/profile/edit');
+        // validationをかける
+        $this->validate($request, Profile::$rules);
+        // Profile Model から データを取得する
+        $profile = Profile::find($request->id);
+        
+        // 送信されてきたフォームデータを格納する
+        $profile_form = $request->all();
+        unset($profile_form['_token']);
+        // 該当するデータを上書きして保存する
+        $profile->fill($profile_form);
+        $profile->save();
+        // 編集履歴を保存する
+        $history = new ProfileHistory();
+        $history->profile_id = $profile->id;
+        $history->edited_at = Carbon::now();
+        $history->save();
+        
+        return redirect('admin/profile');
     }
 }
